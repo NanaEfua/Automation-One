8c9f72267b12a8d893d0900f0ea407d7d9db03ce
NanaEfua <41793369+NanaEfua@users.noreply.github.com>
2022-09-13 12:31:05 +0000

    This is my first commit

---
+++ TestingGroupJ/Features/DataDrivenWithParameter.feature
@@ -0,0 +1,12 @@
+﻿Feature: DataDrivenWithParameter
+	Simple calculator for adding two numbers
+
+@mytag
+Scenario: Data Driven With Parameter
+	Given I navigate to "https://angularjs.realworld.io/#/"
+	And I click on Sign up
+	And I enter my Username "EfuaBo"
+	And I enter my email "nana456"
+	And I enter my password  "4567"
+	When I click on sign up button
+    Then I can register successfully
---
+++ TestingGroupJ/PageObject/RegistrationPage.cs
@@ -0,0 +1,74 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TestingGroupJ.Utilities;
+
+namespace TestingGroupJ.PageObject
+{
+    class RegistrationPage
+    {
+       public RegistrationPage()
+        {
+            driver = Hooks1.driver;
+        }
+
+        IWebDriver driver;
+
+
+        IWebElement SignUp => driver.FindElement(By.XPath("/html/body/div/app-header/nav/div/ul[1]/li[3]/a"));
+        IWebElement Username => driver.FindElement(By.CssSelector("body > div > div > div > div > div > div > form > fieldset > fieldset:nth-child(1) > input"));
+        IWebElement Email => driver.FindElement(By.XPath("/html / body / div / div / div / div / div / div / form / fieldset / fieldset[2] / input"));
+        IWebElement Password => driver.FindElement(By.XPath("/html/body/div/div/div/div/div/div/form/fieldset/fieldset[3]/input"));
+        IWebElement SignUpButton => driver.FindElement(By.CssSelector("body > div > div > div > div > div > div > form > fieldset > button"));
+        IWebElement NewArticle => driver.FindElement(By.XPath("(//a[@class='nav-link'])[3]"));
+       
+        
+
+        public void ClickOnSignUp()
+        {
+            SignUp.Click();
+        }
+
+        public void EnterUsername()
+        {
+            //Random randomGenerator = new Random();
+            //int randomInt = randomGenerator.Next(1000);
+            //Username.SendKeys("EfuaBo" + randomInt);
+            Username.SendKeys("EfuaBo");
+        }
+
+        public void NavigateToWebsite()
+        {
+            driver.Navigate().GoToUrl("https://angularjs.realworld.io/#/");
+        }
+
+        public void EnterEmail()
+        {
+            //Random randomGenerator = new Random();
+            //int randomInt = randomGenerator.Next(1000);
+            //Email.SendKeys("nana" + randomInt + "@yahoo.com");
+            Email.SendKeys("nana@yahoo.com");
+        }
+    
+        public void EnterPassword()
+        {
+            Password.SendKeys("1234");
+        }
+    
+        public void ClickOnSignUpButton()
+        {
+            SignUpButton.Click();
+        }
+
+        public bool IsNewArticleDisplayed()
+        {
+            return NewArticle.Displayed;
+        }
+
+
+
+
+
+    }
+}
